cda096ddf59d643bed1a36ede28abec777df09ea
Mahesh Bhandari <109456925+cricketNepal@users.noreply.github.com>
2022-07-19 18:10:00 +0545

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 const refer=document.querySelector(".mobile-nav");
-const mainho=document.querySelector("#maindivs");
+const main=document.querySelector("#main");
 const nav_head=document.querySelector("header");
 const toogleHead=()=>{
 nav_head.classList.toggle("active");
 }
-mainho.addEventListener('click',()={
-nav_head.classList.remove("active");
-}
+main.addEventListener('click',()=>{
+    nav_head.classList.remove("active");
+})
 refer.addEventListener('click',toogleHead);
-
